d9cff5fe3fe70996b8e98ad6a356f475eebd3288
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-03-28 14:28:07 -0700

    fixed the select

--- views/forms/select_person.php
+++ views/forms/select_person.php
@@ -1,9 +1,9 @@
 <select name="<?php echo  $patient ?>">
-	<?php foreach($people  as $person): ?>
+	<?php foreach($people  as $person): $person = (object) $person; ?>
 		<option 
-		<?php echo  $person->person_id == $person_id ? "selected" : "";?>
-		value="<?php echo  $person->person_id; ?>">
-			<?php echo  $person->person_id.' - '.$person->first_name.', '.$person->last_name; ?>
+		<?php echo  $person->PERSON_ID == $person_id ? "selected" : "";?>
+		value="<?php echo  $person->PERSON_ID; ?>">
+			<?php echo  $person->PERSON_ID.' - '.$person->FIRST_NAME.', '.$person->LAST_NAME; ?>
 		</option>
 	<?php endforeach; ?>
 </select>
