73e908ed95a9c86239b9a76454db9f34574940de
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-03-30 10:37:10 -0700

    Forgot to add gear, also resized

--- views/templates/header.php
+++ views/templates/header.php
@@ -14,7 +14,7 @@
 				<a href="user_settings.php">
 					<?php echo  $user->getUserName() ?>
 				</a>
-				<image width="100" src="/~blaine1/images/gear.png" />
+				<image width="22" src="/~blaine1/images/gear.png" />
 			</div>
 			<a href="logout.php">(logout)</a>
 		<?php endif; ?>
