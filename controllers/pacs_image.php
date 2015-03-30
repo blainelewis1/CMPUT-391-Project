6253d23426950d83a148956511325712bcc25131
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-03-30 12:35:53 -0700

    started validation for uploading images

--- controllers/pacs_image.php
+++ controllers/pacs_image.php
@@ -3,7 +3,9 @@
 function validatePacsImage($filename) {
 	$tokens = split(".", $filename);
 
-	if(!($tokens[sizeof($tokens)] == "jpg" || $tokens[sizeof($tokens)] == "jpeg")) {
+	print_r($tokens);
+
+	if(!($tokens[sizeof($tokens) - 1] == "jpg" || $tokens[sizeof($tokens) - ] == "jpeg")) {
 		return '<div class="error">File extension not supported. Only jpg is supported.</div>';
 	}
 
