115d048a2745c8f935494e05b5e80b0ea8359971
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-04-01 14:54:57 -0700

    moved contains into single clause

--- models/radiology_record.php
+++ models/radiology_record.php
@@ -285,13 +285,14 @@ ORDER BY myrank";
 		oci_bind_by_name($query, ":first_name", $search_term);
 		oci_bind_by_name($query, ":last_name", $search_term);
 
-
 		oci_execute($query);
 
 		print($query_string);
 
 		print_r($query);
 		print(oci_error());
+		print(oci_error($db));
+		print(oci_error($query));
 
 		$results;
 		oci_fetch_all($query, $results, null, null, OCI_ASSOC + OCI_FETCHSTATEMENT_BY_ROW);
