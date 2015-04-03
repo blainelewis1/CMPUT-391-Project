3484b68fcd86e3fd302e9dc134b49b8a60df5c76
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-04-03 13:30:57 -0700

    fxing images

--- views/lists/search.php
+++ views/lists/search.php
@@ -32,11 +32,11 @@
 <script type="text/javascript">
 	function ifSearchTerm(input) {
 			var selectRelevance = document.querySelectorAll("input[value=Relevance]")[0];
-			selectRelavance.disabled = input.value == "";
+			selectRelavance.disabled = input.value == "" ? 'hidden' : 'visible';
 	}
 	
 	var selectRelevance = document.querySelectorAll("input[value=Relevance]")[0];
-	selectRelavance.disabled = <?php echo  $search_term == "" ? 'true' : 'false';
+	selectRelavance.style.visibility = <?php echo  $search_term == "" ? 'hidden' : 'visible';
 
 </script>
 
