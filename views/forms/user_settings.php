330d0cf2240ff80fb553869cae68e2ce83d109a7
Blaine Lewis <blaine.lewis.1@outlook.com>
2015-04-03 12:53:22 -0700

    added in the moves

---
+++ views/forms/user_settings.php
@@ -0,0 +1,8 @@
+<div id="personal_information">
+
+	<?php include("views/forms/person.php"); ?>
+	
+	<div class="light-separator"></div>
+	
+	<?php include('views/forms/change_password.php'); ?>
+</div>
---
+++ views/lists/data_analysis.php
@@ -0,0 +1,52 @@
+<?php include('misc/form_utils.php'); ?>
+
+<form action="" method="GET">
+	<?php echo checkboxes('Analyze', 
+			RadiologyRecord::ANALYZE_LEVEL,
+			array_keys(RadiologyRecord::$ANALYZE_COLUMNS),
+			$columnNames, 
+			"ifDate(this)");
+	?>
+
+	<?php echo selectItems('Drill To',
+				RadiologyRecord::DRILL_LEVEL,
+				array_keys(RadiologyRecord::$DRILL_VALUES),
+				$drill_level);
+	?>
+	<input type="submit" name="<?php echo RadiologyRecord::SEARCH;?>" value="Search" />
+</form>
+
+<script type="text/javascript">
+	function ifDate(checkbox) {
+		if(checkbox.value == "Test Date"){
+			var select = document.getElementsByName("<?php echo RadiologyRecord::DRILL_LEVEL; ?>")[0];
+			select.visible = checkbox.checked;
+		}
+	}
+	
+	var select = document.getElementsByName("<?php echo RadiologyRecord::DRILL_LEVEL; ?>")[0];
+	select.visible = <?php echo in_array("Test Date", $columnNames);?>;
+
+</script>
+
+<?php if(sizeof($rows) == 0): ?>
+	<div class="error">Sorry no totals were found!</div>
+<?php else: ?>
+
+<table>
+
+	<?php foreach($columnNames as $columnName): ?>
+		<th><?php echo $columnName;?></th>
+	<?php endforeach; ?>
+
+	<?php foreach($rows as $row): ?>
+		<tr>
+			<?php foreach($row as $column): ?>
+				<td><?php echo $column; ?></td>
+			<?php endforeach; ?>
+		</tr>
+	<?php endforeach; ?>
+
+</table>
+
+<?php endif; ?>
